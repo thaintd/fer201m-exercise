e60a6291ba57205e82b71519530dd0d83fe2e985
thaintd <114746768+thaintd@users.noreply.github.com>
2023-09-25 20:21:41 +0700

    do exercise

--- src/App.js
+++ src/App.js
@@ -6,45 +6,47 @@ import Card from "./components/carrd/Card";
 function App() {
   return (
     <div className="App">
-      <section className='section-1 mt-5'>
+      <div className="container">
+        <div className="row g-4  ">
+          <div className="col-md-2 col-sm-6">
+            <Button value="Problem Solving" />
+          </div>
+          <div className="col-md-2 col-sm-6">
+            <Button value="Live Chat" />
+          </div>
+          <div className="col-md-2 col-sm-6">
+            <Button value="Group Reading" />
+          </div>
+          <div className="col-md-2 col-sm-6">
+            <Button value="!0k Course" />
+          </div>
+          <div className="col-md-2 ">
+            <Button value="Hand-on Active" />
+          </div>
+        </div>
         <div className="container">
-          <div className='row'>
-            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
-                <h4>col 1</h4>
+          <div className="row g-5 ">
+            <div className="col-md-4 col-sm-6">
+              <Card />
             </div>
-            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
-                <h4>col 2</h4>
+            <div className="col-md-4 col-sm-6">
+              <Card />
             </div>
-            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
-                <h4>col 3</h4>
+            <div className="col-md-4 col-sm-6">
+              <Card />
             </div>
-            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
-                <h4>col 4</h4>
+            <div className="col-md-4 col-sm-6">
+              <Card />
             </div>
-            <div className='col-6 col-sm-6 col-md-6 col-lg-4'>
-                <h4>col 5</h4>
+            <div className="col-md-4 col-sm-6">
+              <Card />
             </div>
-            <div className='col-6 col-sm-6 col-md-6 col-lg-4'>
-                <h4>col 6</h4>
+            <div className="col-md-4 col-sm-6">
+              <Card />
             </div>
           </div>
         </div>
-      </section>
-
-      {/* <hr /> */}
-
-      {/* <Button value="FER201m" />
-      <Button value="FER201m" />
-      <Button value="FER201m" />
-      <Button value="FER201m" /> */}
-      {/* <hr /> */}
-      {/* <div className="list-card" style={{padding: "30px"}}>
-        <Card title="Test" />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div> */}
+      </div>
     </div>
   );
 }
